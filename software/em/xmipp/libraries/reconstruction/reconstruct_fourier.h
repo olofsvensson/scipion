002c469352fca9ba919e3f36c9180fe9f247d341
DStrelak <david.strelak@seznam.cz>
2017-08-08 16:24:12 +0200

    cleaning done

--- software/em/xmipp/libraries/reconstruction/reconstruct_fourier.h
+++ software/em/xmipp/libraries/reconstruction/reconstruct_fourier.h
@@ -97,27 +97,35 @@ public:
      * Method will load data, process them and store result to final destination.
      */
     void run();
+
     /**
      * Method will take data stored in tempVolume and tempWeights
      * (which should be cropped in X axis), calculate IFFT and store result
      * to final destination.
      */
     void finishComputations( const FileName &out_name );
+
     /** Functions of common reconstruction interface */
     virtual void setIO(const FileName &fn_in, const FileName &fn_out);
 
 protected:
 // 	FIELDS
+
     /** Thread used for loading input data */
     LoadThreadParams loadThread;
+
     /** SelFile containing all projections */
     MetaData SF;
+
     /** Output file name */
     FileName fn_out;
+
     /** Input file name */
     FileName fn_in;
+
     /** maximal index in the temporal volumes, Y and Z axis */
 	int maxVolumeIndexYZ;
+
     /** maximal index in the temporal volumes, X axis */
 	int maxVolumeIndexX;
 
@@ -128,11 +136,13 @@ protected:
 	 * sphere within a cube.
 	 */
 	std::complex<float>*** tempVolume = NULL;
+
 	/**
 	 * 3D volume holding the weights of the Fourier coefficients stored
 	 * in tempVolume.
 	 */
 	float*** tempWeights = NULL;
+
 	/**
 	 * Method will take temp spaces (containing complex conjugate values
 	 * in the 'right X side'), transfer them to 'left X side' and remove
@@ -140,35 +150,44 @@ protected:
 	 * will be half of the original.
 	 */
     void mirrorAndCropTempSpaces();
+
     /**
      * Method will enforce Hermitian symmetry, i.e will remove make sure
      * that the values in temporal space at X=0 are complex conjugate of
      * in respect to center of the space
      */
     void forceHermitianSymmetry();
+
     /**
      * Method will in effect do the point-wise division of
      * tempVolume and tempWeights
      * (i.e. correct Fourier coefficients by proper weight)
      */
     void processWeights();
+
     /**
      * Method will create thread used for loading images
      * and set all necessary values/variables
      */
     void createLoadingThread();
+
     /**
      * Method will release all resources allocated for loading images
      */
     void cleanLoadingThread();
+
     /** Read arguments from command line */
     void readParams();
+
     /** Specify supported command line arguments */
     void defineParams();
+
     /** Show basic info to standard output */
     void show();
+
     /** Method will fill other help structures and variables */
     void produceSideinfo();
+
     /**
      * Method will initialize temporal storage (if necessary),
      *  processes images in given interval (including boundary indexes)
@@ -178,66 +197,94 @@ protected:
 
 private:
 //    FIELDS
+
     /** File with symmetries */
     FileName fn_sym;
+
     /** Flag whether to use the weights in the image metadata */
     bool do_weights;
+
     /** If true, blobing is done at the end of the computation */
     bool useFast;
+
     /** Use CTF */
     bool useCTF;
+
     /** True if the images have been already phase flipped */
     bool isPhaseFlipped;
+
     /** Minimum CTF value to invert */
     double minCTF;
+
     /** Inverse of the sampling rate */
     double iTs;
+
     /** Projection padding Factor */
     double padding_factor_proj;
+
     /** Volume padding Factor */
     double padding_factor_vol;
+
     /** Sampling rate in Angstroms/pixel */
     double sampling_rate;
+
     /** Max resolution in Angstroms */
     double maxResolution;
+
     /** Maximum interesting resolution squared */
     double maxResolutionSqr;
+
     /** Tells the loading thread what to do next */
     int threadOpCode;
+
     /** Barrier synchronization for threads */
     barrier_t barrier;
+
     /** Table with blob values, linear sampling */
     Matrix1D<double>  Fourier_blob_table;
+
     /** Table with blob values, squared sampling */
     Matrix1D<double> blobTableSqrt;
+
     /** Inverse of the delta and deltaFourier used in the tables */
     double iDeltaFourier, iDeltaSqrt;
+
     /** Definition of the blob */
     struct blobtype blob;
+
     /** Vector with R symmetry matrices */
     std::vector <Matrix2D<double> > R_repository;
+
     /** Size of the original projection, must be a square */
     int imgSize;
+
     /** Size of the image including padding. Image must be a square */
     int paddedImgSize;
+
     /** Size of loading buffer (i.e. number of projection loaded in one buffer) */
     int bufferSize;
 
 // STATIC METHODS
+
     /** Method to allocate 3D array (not continuous) of given size */
     template<typename T>
     static T*** allocate(T***& where, int xSize, int ySize, int zSize);
+
     /** Method to release 3D array of given size */
     template<typename T>
     void release(T***& array, int ySize, int zSize);
+
     /** Method running on separate thread */
 	static void* loadImageThread(void* threadArgs);
+
 	/** Function behaving like an identity, i.e returning passed value */
 	template<typename T>
 	static T identity(T val) { return val;}; // if used with some big type, use reference
+
 	/** Function returning conjugate of a complex number */
 	template<typename T>
 	static std::complex<T> conjugate(std::complex<T> f) { return conj(f);};
+
     /**
      * Method will process the 'paddedFourier' (not shifted, i.e. low frequencies are in corners)
      * in the following way:
@@ -250,9 +297,11 @@ private:
     static Array2D<std::complex<float> >* cropAndShift(
     		MultidimArray<std::complex<double> >& paddedFourier,
     		ProgRecFourier* parent);
+
     /** Returns value within the range (included) */
     template<typename T, typename U>
     static U clamp(U val, T min, T max);
+
     /**
     *          7____6
     *         3/___2/
@@ -264,16 +313,20 @@ private:
     * otherwise the values can go to negative values
     */
     static void createProjectionCuboid(Point3D* cuboid, float sizeX, float sizeY, float blobSize);
+
     //* Apply rotation transform to cuboid */
     static void rotateCuboid(Point3D* cuboid, const float transform[3][3]) {
     	for (int i = 0; i < 8; i++) {
     		multiply(transform, cuboid[i]);
     	}
     }
+
     /** Do 3x3 x 1x3 matrix-vector multiplication */
     static void multiply(const float transform[3][3], Point3D& inOut);
+
     /** Add 'vector' to each element of 'cuboid' */
     static void translateCuboid(Point3D* cuboid, Point3D vector);
+
     /**
      * Method will calculate Axis Aligned Bound Box of the cuboid and restrict
      * its maximum size
@@ -281,39 +334,51 @@ private:
     static void computeAABB(Point3D* AABB, Point3D* cuboid,
     		float minX, float minY, float minZ,
     		float maxX, float maxY, float maxZ);
+
     /** Returns true if x is in (min, max) interval */
     template <typename T>
     static bool inRange(T x, T min, T max) {
     	return (x > min) && (x < max);
     }
+
     /** Returns X coordinate of the point [y, z] on the plane defined by p0 (origin) and two vectors */
     static bool getX(float& x, float y, float z, const Point3D& a, const Point3D& b, const Point3D& p0);
+
     /** Returns Y coordinate of the point [x, z] on the plane defined by p0 (origin) and two vectors */
     static bool getY(float x, float& y, float z, const Point3D& a, const Point3D& b, const Point3D& p0);
+
     /** Returns Z coordinate of the point [x, y] on the plane defined by p0 (origin) and two vectors */
     static bool getZ(float x, float y, float& z, const Point3D& a, const Point3D& b, const Point3D& p0);
+
     /** Method returns vectors defining the plane */
     static void getVectors(const Point3D* plane, Point3D& u, Point3D& v);
+
     /** DEBUG ONLY method, prints AABB to std::cout. Output can be used in e.g. GNUPLOT */
     static void printAABB(Point3D* AABB);
+
     /** Method will convert Matrix2D matrix to float[3][3] */
     static void convert(Matrix2D<double>& in, float out[3][3]);
+
     /** Method to convert temporal space to expected (original) format */
     template<typename T, typename U>
     static void convertToExpectedSpace(T*** input, int size,
     	MultidimArray<U>& VoutFourier);
 
 // METHODS
-	/** Method will set indexes of the images to load and open sync barrier */
+
+    /** Method will set indexes of the images to load and open sync barrier */
     void loadImages(int startIndex, int endIndex);
+
     /** Method swaps buffers of the loading thread */
     void swapLoadBuffers();
+
     /**
      * Method will use data stored in the buffer and update temporal
      * storages appropriately.
      */
     void processBuffer(ProjectionData* buffer);
-	/**
+
+    /**
 	 * Method will take input array (of size
 	 * maxVolumeIndexYZ*maxVolumeIndexYZ*maxVolumeIndexYZ
 	 * and transfer data to newly created array of size
@@ -326,6 +391,7 @@ private:
 	 */
     template<typename T>
     void mirrorAndCrop(T***& input,T (*f)(T));
+
     /**
      * Method will apply blob to input 3D array.
      * Original array will be released and new (blurred) will be returned
@@ -333,6 +399,7 @@ private:
     template<typename T>
     T*** applyBlob(T***& input, float blobSize,
     		Matrix1D<double>& blobTableSqrt, float iDeltaSqrt);
+
     /**
      * Method will allocate space for output Fourier transformation.
      * If space is already allocated, method will have no effect
@@ -342,6 +409,7 @@ private:
     		VoutFourier.initZeros(paddedImgSize, paddedImgSize, paddedImgSize/2 +1);
     	}
     }
+
     /**
      * Method will process one projection image and add result to temporal
      * spaces. Method also shows progress of the calculation.
@@ -350,6 +418,7 @@ private:
     	ProjectionData* projectionData,
     	const float transform[3][3],
     	const float transformInv[3][3]);
+
     /**
      * Method will map one voxel from the temporal
      * spaces to the given projection and update temporal spaces
@@ -359,6 +428,7 @@ private:
     		int x, int y, int z,
 			const float transform[3][3], float maxDistanceSqr,
     		ProjectionData* const data);
+
     /**
      * Method will map one voxel from the temporal
      * spaces to the given projection and update temporal spaces
@@ -366,6 +436,7 @@ private:
      */
     void processVoxelBlob(int x, int y, int z, const float transform[3][3], float maxDistanceSqr,
     		ProjectionData* const data);
+
     /**
      * Method sets wCTF and wModulator based on position
      * in the image
